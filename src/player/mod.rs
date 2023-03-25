69036d7f2278884e31156263aaf354343b4d61d8
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-25 12:12:58 +0100

    chg use system sets to order system execution time

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -5,15 +5,23 @@ mod systems;
 
 use systems::*;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
+            .configure_set(MovementSystemSet.before(ConfinementSystemSet))
             // Startup Systems
             .add_startup_system(spawn_player)
             // Systems
-            .add_systems((player_movement, confine_player_movement).chain())
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(player_enemy_collision)
             .add_system(player_star_collision);
     }
