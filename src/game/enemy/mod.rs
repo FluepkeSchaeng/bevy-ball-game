b5b79be9a62c1826ce41c20aa33e7eb3efe23095
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-25 14:16:08 +0100

    chg only update enemy movement and spawning of enemies when we are in a running game

--- src/game/enemy/mod.rs
+++ src/game/enemy/mod.rs
@@ -7,6 +7,10 @@ mod systems;
 use resources::*;
 use systems::*;
 
+use crate::AppState;
+
+use super::SimulationState;
+
 pub const NUMBER_OF_ENEMIES: usize = 4;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const ENEMY_SIZE: f32 = 64.0; // enemy sprite size
@@ -15,12 +19,22 @@ pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<EnemySpawnTimer>()
+        app
+            // Resources
+            .init_resource::<EnemySpawnTimer>()
+            // Startup Systems
             .add_startup_system(spawn_enemies)
-            .add_system(enemy_movement)
-            .add_system(update_enemy_direction)
-            .add_system(confine_enemy_movement)
-            .add_system(tick_enemy_spawn_timer)
-            .add_system(spawn_enemies_over_time);
+            // Systems
+            .add_systems(
+                (
+                    enemy_movement,
+                    update_enemy_direction,
+                    confine_enemy_movement,
+                    tick_enemy_spawn_timer,
+                    spawn_enemies_over_time,
+                )
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(SimulationState::Running)),
+            );
     }
 }
