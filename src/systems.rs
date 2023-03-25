11d8fcbec74845045b9dad3b3567434c954f6688
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-25 15:27:12 +0100

    chg using states with our Score Plugin

--- src/systems.rs
+++ src/systems.rs
@@ -2,7 +2,7 @@ use bevy::app::AppExit;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
-use crate::{events::*, AppState, game::SimulationState};
+use crate::{events::*, game::SimulationState, AppState};
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
@@ -49,8 +49,9 @@ pub fn exit_game(
     }
 }
 
-pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
     for event in game_over_event_reader.iter() {
         println!("Your final score is: {}", event.score.to_string());
+        commands.insert_resource(NextState(Some(AppState::GameOver)));
     }
 }
