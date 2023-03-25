6137b82d7e509aedf78e208229248c05841dab66
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-25 12:18:19 +0100

    chg use intermediate construct of system sets for now

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -5,28 +5,23 @@ mod systems;
 
 use systems::*;
 
-//#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-//pub struct MovementSystemSet;
-
-//#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-//pub struct ConfinementSystemSet;
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
 
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-pub enum PlayerSystemSet {
-    Movement,
-    Confinement,
-}
+pub struct ConfinementSystemSet;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.configure_set(PlayerSystemSet::Movement.before(PlayerSystemSet::Confinement))
+        app
+            .configure_set(MovementSystemSet.before(ConfinementSystemSet))
             // Startup Systems
             .add_startup_system(spawn_player)
             // Systems
-            .add_system(player_movement.in_set(PlayerSystemSet::Movement))
-            .add_system(confine_player_movement.in_set(PlayerSystemSet::Confinement))
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(player_enemy_collision)
             .add_system(player_star_collision);
     }
