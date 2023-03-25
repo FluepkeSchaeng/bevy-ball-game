4ba4e8c670277be51c6bd82fd56ca71817ce6460
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-25 12:05:39 +0100

    chg let confine player movement run after player movement, so the movement does not stutter on edges

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -9,9 +9,11 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+        app
+            // Startup Systems
+            .add_startup_system(spawn_player)
+            // Systems
+            .add_systems((player_movement, confine_player_movement).chain())
             .add_system(player_enemy_collision)
             .add_system(player_star_collision);
     }
