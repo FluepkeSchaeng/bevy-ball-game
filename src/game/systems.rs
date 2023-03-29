2f342f66088d84ad9170e9371b809c0eed4b28ad
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-29 14:47:08 +0200

    chg api call for setting next AppState to preferred one

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -11,17 +11,17 @@ pub fn resume_simulation(mut simulation_state_next_state: ResMut<NextState<Simul
 }
 
 pub fn toggle_simulation(
-    mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     simulation_state: Res<State<SimulationState>>,
+    mut simulation_state_next_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         if simulation_state.0 == SimulationState::Running {
-            commands.insert_resource(NextState(Some(SimulationState::Paused)));
+            simulation_state_next_state.set(SimulationState::Paused);
             println!("Simulation Paused.");
         }
         if simulation_state.0 == SimulationState::Paused {
-            commands.insert_resource(NextState(Some(SimulationState::Running)));
+            simulation_state_next_state.set(SimulationState::Running);
             println!("Simulation Running.")
         }
     }
