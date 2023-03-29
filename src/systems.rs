f78f06dea141271244c6c52c45dbc98d6a74e21c
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-29 14:41:31 +0200

    moved fn handle_game_over before fn exit_game to clean up code

--- src/systems.rs
+++ src/systems.rs
@@ -42,18 +42,18 @@ pub fn transition_to_main_menu_state(
     }
 }
 
+pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
+    for event in game_over_event_reader.iter() {
+        println!("Your final score is: {}", event.score.to_string());
+        commands.insert_resource(NextState(Some(AppState::GameOver)));
+    }
+}
+
 pub fn exit_game(
     keyboard_input: Res<Input<KeyCode>>,
     mut app_exit_event_writer: EventWriter<AppExit>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
         app_exit_event_writer.send(AppExit);
-    }
-}
-
-pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
-    for event in game_over_event_reader.iter() {
-        println!("Your final score is: {}", event.score.to_string());
-        commands.insert_resource(NextState(Some(AppState::GameOver)));
     }
 }
