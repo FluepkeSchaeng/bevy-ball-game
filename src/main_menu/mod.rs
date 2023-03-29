3044d70ebc217cf14df0e359110e1e3f1fce7610
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-29 15:28:18 +0200

    add mew modules for main_menu GUI and some files to implement it

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,13 +1,21 @@
+mod components;
+mod styles;
+mod systems;
+
+use systems::layout::*;
+
 use bevy::prelude::*;
+
+use crate::AppState;
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(main_menu);
+        app
+            // OnEnter State Systems
+            .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+            // OnExit State Systems
+            .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }
-}
-
-pub fn main_menu() {
-    println!("You are on the main menu.")
 }
